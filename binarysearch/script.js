8327a0bf14a12f179631db946ccbc50858db8d08
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2024-10-21 18:45:20 +0200

    added binarysearch r

---
+++ binarysearch/script.js
@@ -0,0 +1,41 @@
+"use strict"
+window.addEventListener("load", main);
+
+function main() {
+    console.log("Hello");
+    
+}
+
+const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
+
+function binarySearch(search, values) {
+    let min = 0;
+    let max = values.length
+    let middle;
+    let found = false
+
+    while (!found && min < max) {
+        middle = min + Math.floor((max - min)/2)
+        let com = compare(search, values[middle])
+
+        if (com == 0) {
+            found = true
+            return middle
+        } else if (com < 0) {
+            console.log("Det var for højt");
+            max = middle -1
+                
+        } else if(com > 0) {
+            console.log("Det var for lavt");
+            min = middle + 1
+        }
+    }
+    return -1;
+}
+
+function compare(num1, num2) {
+    return num1 - num2
+}
+
+let index = binarySearch(23, values);
+console.log(`Talet fundet på ${index}`);
